3b564a12efc4cb763693ab6075db3db4550ff999
Jesse Meyer <twincitiesjesse@gmail.com>
2025-08-12 20:41:13 -0500

    Improve file search command for components
    Replaced ls with find - should handle component file names with
    spaces now.
    
    Also some general comment cleanup

--- bash_profile.bash
+++ bash_profile.bash
@@ -9,16 +9,18 @@ case $- in
 esac
 
 ### General Configuration ###
-HAS_NERD=1	# Nerd fonts
+HAS_NERD=1			# Nerd fonts
+BASH_CONFIG=~/.config/bash	# Location of config files
 
-BASH_CONFIG=~/.config/bash/
-
-files=( $(ls -1 ${BASH_CONFIG}/components | sort) )
-
-# Source all Bash configuration files from the components directory.
+# For organization, readability, and maintenance's sake, the various Bash
+# configuration options are split into different files and placed in the
+# components directory.
+#
+# This section sourcces all files with the *.bash extension in that directory
+files=( $(find "${BASH_CONFIG}/components" -maxdepth 1 -type f -name "*.bash") )
 for file in "${files[@]}"; do
-    if [ -f "${BASH_CONFIG}/components/$file" ]; then
-	source "${BASH_CONFIG}/components/$file"
+    if [ -f "$file" ]; then
+	source "$file"
     fi
 done
 
@@ -34,4 +36,3 @@ fi
 if [ -f ~/.bashrc_private ]; then
     . ~/.bashrc_private
 fi
-
